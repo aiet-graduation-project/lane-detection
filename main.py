6389d93cf72ccd0c27e2008c71d7df9e41f6e5cd
Ziad <ziadhesham280@gmail.com>
2021-12-31 11:06:31 +0200

    grayscale

--- main.py
+++ main.py
@@ -1,2 +1,28 @@
 import cv2
 import numpy as np
+
+# read the input video
+cap = cv2.VideoCapture("test2.mp4")
+
+
+def detect_lane(image):
+    # convert current frame into grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    return gray
+
+
+# show the ouptut video
+while cap.isOpened():
+    # Capture video frames
+    _, frame = cap.read()
+    # take a copy from current frame
+    lane_image = frame
+    result = detect_lane(lane_image)
+    # show the output frame
+    cv2.imshow('result', result)
+    # break the loop when the Q key pressed
+    if cv2.waitKey(1) == ord('q'):
+        break
+# close output windows
+cap.release()
+cv2.destroyAllWindows()
